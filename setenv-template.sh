e5d8f0fecd618fdf011e2ab419a381692701a0c7
Robert Reris <rreris@fortinet.com>
2025-03-17 15:46:41 -0700

    removing Jenkins cli jar env var

--- setenv-template.sh
+++ setenv-template.sh
@@ -3,5 +3,4 @@ export GITHUB_TOKEN=mytoken123
 export JENKINS_URL=https://jenkinsurl.com:8443
 export JENKINS_USER_ID=myusername
 export JENKINS_API_TOKEN=abcd1234
-export JENKINS_CLI_JAR=/path/to/jenkins-cli.jar
 export FDS_TOKEN=abceasyas123orsimpleasdoremi
